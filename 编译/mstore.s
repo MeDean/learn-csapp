95d2b48351fc6a7a0250e51dec718f156dbe8453
MeDean <852739386@qq.com>
2018-02-11 19:05:20 +0800

    编译演示代码

---
+++ 编译/mstore.s
@@ -0,0 +1,34 @@
+	.file	"mstore.c"
+	.text
+	.globl	_Z9multstorellPl
+	.type	_Z9multstorellPl, @function
+_Z9multstorellPl:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movq	%rdi, -24(%rbp)
+	movq	%rsi, -32(%rbp)
+	movq	%rdx, -40(%rbp)
+	movq	-32(%rbp), %rdx
+	movq	-24(%rbp), %rax
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_Z5mult2ll
+	movq	%rax, -8(%rbp)
+	movq	-40(%rbp), %rax
+	movq	-8(%rbp), %rdx
+	movq	%rdx, (%rax)
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	_Z9multstorellPl, .-_Z9multstorellPl
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.6) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
